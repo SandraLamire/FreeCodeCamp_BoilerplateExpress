59f3eb0f851f4a3c26cb043d45ea81a5da572ac5
SandraLamire <sandra.lamire.sl@gmail.com>
2023-08-24 18:52:36 +0200

    #1 - Install NodeExpress

--- myApp.js
+++ myApp.js
@@ -1,6 +1,8 @@
+// lancer npm install express puis
+// lancer l'affichage dans le terminal avec node myApp.js
 let express = require('express');
 let app = express();
-
+console.log("Hello World");
 
 
 
